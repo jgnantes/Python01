8f6797d82b91166d4dfdb820a0daccc5bde9321e
jnantes- <jgnantes99@outlook.com>
2026-01-19 14:50:18 -0300

    ex0 feito

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+    print("=== Welcome to My Garden ===")
+    plant = "Rose"
+    print(f"Plant: {plant}")
+    height = 25
+    print(f"Height: {height}cm")
+    age = 30
+    print(f"Age: {age} days")
+    print("\n")
+    print("=== End of Program ===")
