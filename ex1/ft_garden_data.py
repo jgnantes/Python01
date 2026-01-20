9b86851f18452e4b111aa7ae0710798b2fcf2c29
jnantes- <jgnantes99@outlook.com>
2026-01-20 14:19:36 -0300

    ex1 feito

--- ex1/ft_garden_data.py
+++ ex1/ft_garden_data.py
@@ -1,7 +1,14 @@
-class Plant
+class Plant:
     def __init__(self, name, height, age):
         self.name = name
         self.height = height
         self.age = age
 
 if __name__ == "__main__":
+    print("=== Garden Plant Registry ===")
+    Rose = Plant("Rose", 25, 30)
+    Sunflower = Plant("Sunflower", 80, 45)
+    Cactus = Plant("Cactus", 15, 120)
+    print(f"{Rose.name}: {Rose.height}cm, {Rose.age} days old")
+    print(f"{Sunflower.name}: {Sunflower.height}cm, {Sunflower.age} days old")
+    print(f"{Cactus.name}: {Cactus.height}cm, {Cactus.age} days old")
