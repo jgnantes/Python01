7550336bfe716a31adb489d8da1796659ba8ab5a
jgnantes <jgnantes@outlook.com>
2026-01-20 20:40:53 -0300

    ex2 feito? não sei

--- ex2/ft_plant_growth.py
+++ ex2/ft_plant_growth.py
@@ -3,21 +3,18 @@ class Plant:
         self.name = name
         self.height = height
         self.age = age
+    def grow(self, growth):
+        self.height += growth
+    def age_week(self):
+        self.age += 6
 
 if __name__ == "__main__":
     Rose = Plant("Rose", 25, 30)
     growth = 6
-
-    def grow(Plant, growth):
-        Plant.height += growth
-
-    def age(Plant):
-        Plant.age = Plant.age + 6
-
     print("=== Day 1 ===")
     print(f"{Rose.name}: {Rose.height}cm, {Rose.age} days old")
-    grow(Rose, growth)
-    age(Rose)
+    Rose.grow(growth)
+    Rose.age_week()
     print("=== Day 7 ===")
     print(f"{Rose.name}: {Rose.height}cm, {Rose.age} days old")
     print(f"Growth this week: +{growth}cm")
