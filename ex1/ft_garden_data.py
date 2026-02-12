9ff6c29d311b6a0be665af112e4558ee17d89bbc
João G. Nantes <jgnantes@outlook.com>
2026-02-11 22:38:33 -0300

    c

--- ex1/ft_garden_data.py
+++ ex1/ft_garden_data.py
@@ -1,6 +1,6 @@
 class Plant:
     """Represents a plant in the garden with basic attributes"""
-    def __init__(self, name, height, age):
+    def __init__(self, name: str, height: int, age: int):
         """Initializes a Plant with name, height (cm), and age (days)"""
         self.name = name
         self.height = height
